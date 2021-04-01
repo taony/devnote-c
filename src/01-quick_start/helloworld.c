5cfde891f9f3800c40158be5ff88a38d5aa91c94
taony <taonyzhang@gmail.com>
2021-04-01 23:59:01 +0800

    main

--- src/01-quick_start/helloworld.c
+++ src/01-quick_start/helloworld.c
@@ -2,9 +2,13 @@
 #include <stdio.h>
 
 int main( ){
+    
     int a;
+    
     printf("Plase Input You Age: \n ");
     scanf("%d",&a);
+    
     printf("Your input char is: %d \n ",a);
     return a;
+
 }
